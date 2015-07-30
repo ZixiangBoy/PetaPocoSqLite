d81849853c1f0270d14e19b7173a6c0b7d6325db
ky-srx <ky-srx@bitbucket.org>
2015-07-30 14:00:41 +0800

    Srx

---
+++ PetaPocoSqLite/Program.cs
@@ -0,0 +1,27 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UltraDbEntity;
+
+namespace PetaPocoDemo {
+    class Program {
+        static void Main(string[] args) {
+
+            using (var db = new Database("UltraDbEntity")) {
+                var user = new t_user() { 
+                    CreateDate=DateTime.Now,
+                    Guid=Guid.NewGuid(),
+                    Creator="admin",
+                    IsUsing=true,
+                    Pwd="123",
+                    UserName="admin"
+                };
+
+                var result=user.Insert();
+
+            }
+        }
+    }
+}
